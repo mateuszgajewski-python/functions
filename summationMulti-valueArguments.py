fbf4a78068f841a30b468cfb1c4d525937a23ece
Mateusz Gajewski <mateuszgajewski@gmail.com>
2021-01-28 12:20:29 +0100

    21.01.28

---
+++ summationMulti-valueArguments.py
@@ -0,0 +1,7 @@
+def dodawnie(*arg):
+    print(*arg) #argumenty
+    print(arg)  #krotka armgumentow
+    print(sum(arg)) #sumowanie wartosci elementow krotki
+    
+
+dodawnie(1,2,3,4,12,43,12,54,65,2,45,234,65)
